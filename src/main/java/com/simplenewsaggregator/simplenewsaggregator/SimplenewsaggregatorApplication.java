90e1048630a30203ca0637abdbde1f3960366520
Boris <bcy@host.voo.be>
2024-11-04 10:41:05 +0100

    init

---
+++ src/main/java/com/simplenewsaggregator/simplenewsaggregator/SimplenewsaggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.simplenewsaggregator.simplenewsaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplenewsaggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplenewsaggregatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplenewsaggregator/simplenewsaggregator/SimplenewsaggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplenewsaggregator.simplenewsaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplenewsaggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
