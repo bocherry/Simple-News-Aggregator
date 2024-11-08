c58b6c701c5a79e8f191f500a9ce115745ae2dd5
bocherry <boris.cherry@hotmail.com>
2024-11-08 17:01:26 +0100

    Lombok setup

--- src/main/java/com/simplenewsaggregator/simplenewsaggregator/models/Publisher.java
+++ src/main/java/com/simplenewsaggregator/simplenewsaggregator/models/Publisher.java
@@ -7,7 +7,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
+import lombok.Data;
 
+@Data
 @Entity
 public class Publisher {
     @Id @GeneratedValue
@@ -22,90 +24,5 @@ public class Publisher {
 
     @OneToMany
     ArrayList<Story> stories;
-
-    public Publisher() {
-    }
-
-    public Publisher(String title, String description, String url, String language) {
-        this.title = title;
-        this.description = description;
-        this.url = url;
-        this.language = language;
-    }
-
-    public String getLanguage() {
-        return this.language;
-    }
-
-    public void setLanguage(String language) {
-        this.language = language;
-    }
-
-    public String getTitle() {
-        return this.title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return this.description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getUrl() {
-        return this.url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public Publisher title(String title) {
-        setTitle(title);
-        return this;
-    }
-
-    public Publisher description(String description) {
-        setDescription(description);
-        return this;
-    }
-
-    public Publisher url(String url) {
-        setUrl(url);
-        return this;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == this)
-            return true;
-        if (!(o instanceof Publisher)) {
-            return false;
-        }
-        Publisher publisher = (Publisher) o;
-        return Objects.equals(title, publisher.title) && Objects.equals(description, publisher.description) && Objects.equals(url, publisher.url);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(title, description, url);
-    }
-
-
-    @Override
-    public String toString() {
-        return "{" +
-            " title='" + getTitle() + "'" +
-            ", description='" + getDescription() + "'" +
-            ", url='" + getUrl() + "'" +
-            ", language='" + getLanguage() + "'" +
-            "}";
-    }
-
 
 }
--- src/main/java/com/simplenewsaggregator/simplenewsaggregator/models/PublisherConfiguration.java
+++ src/main/java/com/simplenewsaggregator/simplenewsaggregator/models/PublisherConfiguration.java
@@ -1,11 +1,12 @@
 package com.simplenewsaggregator.simplenewsaggregator.models;
-import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
+import lombok.Data;
 
+@Data
 @Entity
 public class PublisherConfiguration {
     @Id @GeneratedValue
@@ -18,77 +19,5 @@ public class PublisherConfiguration {
     private Publisher publisher;
 
 
-    public PublisherConfiguration() {
-    }
-
-    public PublisherConfiguration(String RSSUrl, String updatePeriod, int updateFrequency) {
-        this.RSSUrl = RSSUrl;
-        this.updatePeriod = updatePeriod;
-        this.updateFrequency = updateFrequency;
-    }
-
-    public String getRSSUrl() {
-        return this.RSSUrl;
-    }
-
-    public void setRSSUrl(String RSSUrl) {
-        this.RSSUrl = RSSUrl;
-    }
-
-    public String getUpdatePeriod() {
-        return this.updatePeriod;
-    }
-
-    public void setUpdatePeriod(String updatePeriod) {
-        this.updatePeriod = updatePeriod;
-    }
-
-    public int getUpdateFrequency() {
-        return this.updateFrequency;
-    }
-
-    public void setUpdateFrequency(int updateFrequency) {
-        this.updateFrequency = updateFrequency;
-    }
-
-    public PublisherConfiguration RSSUrl(String RSSUrl) {
-        setRSSUrl(RSSUrl);
-        return this;
-    }
-
-    public PublisherConfiguration updatePeriod(String updatePeriod) {
-        setUpdatePeriod(updatePeriod);
-        return this;
-    }
-
-    public PublisherConfiguration updateFrequency(int updateFrequency) {
-        setUpdateFrequency(updateFrequency);
-        return this;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == this)
-            return true;
-        if (!(o instanceof PublisherConfiguration)) {
-            return false;
-        }
-        PublisherConfiguration publisherConfiguration = (PublisherConfiguration) o;
-        return Objects.equals(RSSUrl, publisherConfiguration.RSSUrl) && Objects.equals(updatePeriod, publisherConfiguration.updatePeriod) && Objects.equals(updateFrequency, publisherConfiguration.updateFrequency);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(RSSUrl, updatePeriod, updateFrequency);
-    }
-
-    @Override
-    public String toString() {
-        return "{" +
-            " RSSUrl='" + getRSSUrl() + "'" +
-            ", updatePeriod='" + getUpdatePeriod() + "'" +
-            ", updateFrequency='" + getUpdateFrequency() + "'" +
-            "}";
-    }
     
 }
