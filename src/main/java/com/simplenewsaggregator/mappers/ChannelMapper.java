c9b2f381aef6eda9efc85244d757485e73afb674
bocherry <boris.cherry@hotmail.com>
2024-11-20 14:20:10 +0100

    fix untrimmed updatePeriod

--- src/main/java/com/simplenewsaggregator/mappers/ChannelMapper.java
+++ src/main/java/com/simplenewsaggregator/mappers/ChannelMapper.java
@@ -4,6 +4,7 @@ import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 
 import com.simplenewsaggregator.simplenewsaggregator.dtos.ChannelDto;
@@ -14,7 +15,7 @@ import com.simplenewsaggregator.simplenewsaggregator.models.Publisher;
 public interface ChannelMapper {
     
     ChannelMapper INSTANCE = Mappers.getMapper(ChannelMapper.class);
-
+    
     @Mapping(target = "url", source = "rssDto.link")
     @Mapping(target = "stories", source = "rssDto.items")
     @Mapping(target = "configuration.rssUrl", source = "urlString")
@@ -22,12 +23,14 @@ public interface ChannelMapper {
     @Mapping(target = "configuration.updateFrequency", source = "rssDto.updateFrequency")
     Publisher channelDtoToPublisher(ChannelDto rssDto, String urlString);
 
-
-    // PublisherConfiguration channelDtoToPublisherConfiguration(ChannelDto rssDto);
-
+    default String trimString(String value) {
+        return value != null ? value.trim() : null;
+    }
     @AfterMapping
     default void channelToDtoPostMapping(ChannelDto source, @MappingTarget Publisher publisher) {
         publisher.getConfiguration().setPublisher(publisher);
+        publisher.getConfiguration().setUpdatePeriod(publisher.getConfiguration().getUpdatePeriod().trim());
         publisher.getStories().forEach((story) -> story.setPublisher(publisher));
     }
+
 }
